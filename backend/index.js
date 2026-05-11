3d3ca0740595b0d8693de70a50bc44334014cc1c
Mohit Tiwari <mohittiwari17091709@gmail.com>
2026-05-12 00:49:29 +0530

    Refactor index.js for improved structure and clarity

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,20 @@
 import express from "express";
 import dotenv from "dotenv";
-import connectDB from "config/db.js";
-import authRoute from "../routes/auth.route.js";
-connectDB();
-dotenv.config();
-const app=express();
-app.use(express.json());
-app.use("/api",authRoute);
+import cookieParser from "cookie-parser";
+import connectDB from "./config/db.js";
+import authRoute from "./routes/auth.route.js";
 
-const port=process.env.PORT|| 5000;
-app.listen(port,()=>{
-  console.log(`port is running at ${port}`)
+dotenv.config();
+connectDB();
+
+const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use("/api/auth", authRoute);
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
