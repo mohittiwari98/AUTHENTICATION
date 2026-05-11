aff02c020764f09e192ae8ad5f6c6395ae873ade
Mohit Tiwari <mohittiwari17091709@gmail.com>
2026-05-11 20:40:18 +0530

    Create user model with schema and export

---
+++ backend/models/user model.js
@@ -0,0 +1,40 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  lastName: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  userName: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    lowercase: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    lowercase: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  profileImage: {
+    type: String,
+    required: false,
+    default: ""
+  }
+}, { timestamps: true });
+
+const User = mongoose.model("User", userSchema);
+export default User;
