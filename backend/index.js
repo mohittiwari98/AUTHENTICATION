85bb0db3d233fddba49142ef58e4dad4b1d0cb93
Mohit Tiwari <mohittiwari17091709@gmail.com>
2026-05-11 21:15:03 +0530

    Integrate authentication routes into the server

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,13 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from "config/db.js";
+import authRoute from "../routes/auth.route.js";
 connectDB();
 dotenv.config();
 const app=express();
 app.use(express.json());
-app.get("/",(req,res)=>{
-  res.send("Hello");
-});
+app.use("/api",authRoute);
+
 const port=process.env.PORT|| 5000;
 app.listen(port,()=>{
   console.log(`port is running at ${port}`)
