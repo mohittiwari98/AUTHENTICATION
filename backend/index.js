905e238b5e25c150ea5c2c44bcd3887b19c227f7
Mohit Tiwari <mohittiwari17091709@gmail.com>
2026-05-11 20:20:20 +0530

    Set up Express server with dotenv configuration

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,12 @@
-
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+const app=express();
+app.use(express.json());
+app.get("/",(req,res)=>{
+  res.send("Hello");
+});
+const port=process.env.PORT|| 5000;
+app.listen(port,()=>{
+  console.log(`port is running at ${port}`)
+});
