bb0495b1f206db795c7aaacb25f7b25a8a7a8745
Mohit Tiwari <mohittiwari17091709@gmail.com>
2026-05-11 21:38:19 +0530

    Refactor signup function to use bcrypt and async/await

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -1,7 +1,24 @@
-export const signup=(req,res)=>{
-  try{
-    
-  }
-  catch(e){
+import bcrypt from "bcryptjs";
+export const signup = async (req, res) => {
+  try {
+    const { firstName, lastName, email, password, userName } = req.body;
+    if (!firstName || !lastName || !email || !password || !userName) {
+      return res.status(400).json({ message: "please fill each details" });
+    }
+    let existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ message: "User already existed" });
+    }
+    const hashedpassword = await bcrypt.hash(password, 10);
+    const user = await User.create({
+      firstName,
+      lastName,
+      email,
+      password: hashedpassword,
+      userName
+    });
+    return res.status(201).json({ message: "User created", user });
+  } catch (e) {
+    return res.status(400).json({ message: "Error in signup" });
   }
 }
