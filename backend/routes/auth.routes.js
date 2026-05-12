c19b018c7cf2abac9251db13eb34f5d36a34a843
Mohit Tiwari <mohittiwari17091709@gmail.com>
2026-05-12 08:36:35 +0530

    Update auth.routes.js

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,6 +1,8 @@
 import express,{Router} from "express";
-import {signup} from "../controllers/auth.controllers.js":
+import {signup} from "../controllers/auth.controllers.js";
+import {Login} from "../controllers/auth.controllers.js";
 autRouter=express(Router());
 authRouter.post("/signup",signup)
+authRouter.post("/login",Login)
 export default authRouter;
   
